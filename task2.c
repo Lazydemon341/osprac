a2c9447a7b6191d0d48437b845dfca03a0fbfb94
Alexander <vlas.s.341@gmail.com>
2021-02-16 14:08:33 +0300

    Lab5 done

--- task2.c
+++ task2.c
@@ -1,44 +1,48 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-  int     fd[2];
-  ssize_t size;
-  char     string[] = "Hello, world!";
-  char  resstring[14];
-  //
-  // Try to create a pipe
-  //
-  if (pipe(fd) < 0) {
-    printf("Can\'t open pipe\n");
-    exit(-1);
-  }
-
-  size = write(fd[1], string, 14);
-
-  if (size != 14) {
-    printf("Can\'t write all string to pipe\n");
-    exit(-1);
-  }
-
-  size = read(fd[0], resstring, 14);
-
-  if (size < 0) {
-    printf("Can\'t read string from pipe\n");
-    exit(-1);
-  }
-
-  printf("%s\n", resstring);
-
-  if (close(fd[0]) < 0) {
-    printf("Can'\t close reading side of pipe\n"); exit(-1);
-  }
-  if (close(fd[1]) < 0) {
-    printf("Can'\t close writing side of  pipe\n"); exit(-1);
-  }
-
-  return 0;
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+int main() { 
+int fd[2], result; 
+int fd1[2]; 
+size_t size; 
+char string[] = "Hello, World!"; 
+char string1[] = "World, Hello!"; 
+char resstring[14]; 
+char resstring1[14]; 
+pipe(fd); 
+pipe(fd1); 
+result = fork(); 
+if(result<0) { 
+  printf("Can\'t fork child\n"); 
+} else if (result > 0) { 
+  close(fd[0]); 
+  write(fd[1], string, 14); 
+  printf("Parent writting string...(pipe 1)\n"); 
+  close(fd[1]); 
+  printf("Parent exit\n"); 
+} else { 
+  close(fd1[0]); 
+  close(fd[1]); 
+  printf("Child reading string...(pipe 1)\n"); 
+  read(fd[0], resstring, 14); 
+  printf("%s\n", resstring); 
+  close(fd[0]); 
+  printf("Child writting string...(pipe 2)\n"); 
+  write(fd1[1], string1, 14); 
+  close(fd1[1]); 
+  printf("Child exit\n"); 
+} 
+ 
+sleep(3); 
+if (result > 0) { 
+  close(fd1[1]); 
+  printf("Parent reading string...(pipe 2)\n"); 
+  read(fd1[0], resstring1, 14); 
+  printf("%s\n", resstring1); 
+  close(fd1[0]); 
+  printf("Parent exit\n"); 
+} 
+ 
+return 0; 
 }
