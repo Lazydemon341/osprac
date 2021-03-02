b177151fac2c1a30a03a92692d49b29985afc2be
Alexander <vlas.s.341@gmail.com>
2021-03-02 14:12:46 +0300

    Some fixes to task3

--- task3.c
+++ task3.c
@@ -10,17 +10,19 @@ int main() {
     int fd[2];
     size_t size = 1;
     char* resstring = malloc(1 * sizeof(char));
+
     if(pipe(fd) < 0) {
-        printf("Can\'t create pipe\n");
+        printf("Failed to create pipe\n");
         exit(-1);
     }
     fcntl(fd[1], F_SETFL, fcntl(fd[1], F_GETFL) | O_NONBLOCK);
-    
+
+    size = write(fd[1], resstring, 1);
     while (size == 1) {
         size = write(fd[1], resstring, 1);
         result++;
     }
-    printf("Result: %d bytes\n", result);
+    printf("Size of pipe: %d bytes\n", result);
     return 0;
 }
 
