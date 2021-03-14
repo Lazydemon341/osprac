cf6312eb4a18a88c3cc1a8b33c0efb1c2217aa92
Alexander <vlas.s.341@gmail.com>
2021-03-14 22:52:10 +0300

    Fix lab7

--- 07-a.c
+++ 07-a.c
@@ -1,47 +1,57 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include <sys/shm.h> // Shared memory
+#include <sys/types.h> 
 #include <stdio.h>
-#include <errno.h>
-int main()
-{
-    int shmid;
-    char pathname[] = "07-a.c";
-    key_t key;
+#include <stdlib.h> 
+
+int main() {
+    int shmid1, shmid2;
+    char pathname1[] = "07-a.c", pathname2[] = "07-b.c";
+    key_t key1, key2;
     char *source_code;
     
-    int file_size = 0;
-    int *file_size_ptr;
+    int size = 0;
+    int *file_ptr1, *file_ptr2;
     FILE *file;
-    file = fopen(pathname, "r");
+    file = fopen(pathname1, "r");
     
-    fseek(file, 0L, SEEK_END);
-    file_size = ftell(file);
+    // Устанавливаем внутренний указатель на конец файла
+    fseek(file, 0, SEEK_END);
+    // Получаем размер файла (в байтах)
+    size = ftell(file);
+    // Устанавливаем указатель на начало файла
     rewind(file);
     
-    if ((key = ftok(pathname,0)) < 0) {
+    if ((key1  = ftok(pathname1, 0))  < 0 ||
+        (key2 = ftok(pathname2, 0)) < 0) {
         printf("Can\'t generate key\n");
         exit(-1);
     }
-    
-    if ((shmid = shmget(key, sizeof(int) + file_size * sizeof(char), 0666|IPC_CREAT)) < 0) {
+    // Создаем сегменты общей памяти (для размера текста и самого текста)
+    if ((shmid1  = shmget(key1,  sizeof(int), 0666|IPC_CREAT)) < 0 ||
+        (shmid2 = shmget(key2, size * sizeof(char), 0666|IPC_CREAT)) < 0) {
         printf("Can\'t create shared memory\n");
         exit(-1);
     }
-    if ((file_size_ptr = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
-        printf("Can't attach shared memory\n");
+    // Прикрепляем сегменты памяти 
+    if ((file_ptr1  = (int*)shmat(shmid1,  NULL, 0)) == (int*)(-1) ||
+        (file_ptr2 = (int*)shmat(shmid2, NULL, 0)) == (int*)(-1)) {
+        printf("Can't attach shared memory for size\n");
         exit(-1);
     }
-    *file_size_ptr = file_size;
-    source_code = (char*)(file_size_ptr + 1);
+	// Размер текста
+    *file_ptr1 = size; 
+	// Текст
+    source_code = (char*)file_ptr2;
     
-    for (int i = 0; i < file_size; i++)
+    for (int i = 0; i < size; ++i) {
         source_code[i] = fgetc(file);
-        
+    }
+	
     fclose(file);
-        
     
-    if (shmdt(file_size_ptr) < 0) {
+    // Открепляем сегменты памяти 
+    if (shmdt(file_ptr1)  < 0 ||
+        shmdt(file_ptr2) < 0) {
         printf("Can't detach shared memory\n");
         exit(-1);
     }
--- 07-b.c
+++ 07-b.c
@@ -1,46 +1,62 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include <sys/shm.h> // Shared memory
+#include <sys/types.h> 
 #include <stdio.h>
-#include <errno.h>
-int main()
-{
+#include <stdlib.h> 
+
+int main() {
     int *array;
-    int shmid;
-    char pathname[] = "07-a.c";
-    key_t key;
-    char *source_code;
+    int shmid1, shmid2;
+    char pathname1[] = "07-a.c", pathname2[] = "07-b.c";
+    key_t key1, key2;
+    char *code;
     
-    int file_size = 0;
-    int *file_size_ptr;
+    int size = 0;
+    int *file_ptr1, *file_ptr2;
     
-    if ((key = ftok(pathname,0)) < 0) {
+    if ((key1  = ftok(pathname1, 0))  < 0 ||
+        (key2 = ftok(pathname2, 0)) < 0) {
         printf("Can\'t generate key\n");
         exit(-1);
     }
-    
-    if ((shmid = shmget(key, sizeof(int) + file_size * sizeof(char), 0)) < 0) {
-        printf("Can\'t create shared memory\n");
+
+    if ((shmid1 = shmget(key1, sizeof(int), 0)) < 0) {
+        printf("Can\'t create shared memory for text size\n");
         exit(-1);
     }
-    if ((file_size_ptr = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
-        printf("Can't attach shared memory\n");
+    if ((file_ptr1 = (int*)shmat(shmid1, NULL, 0)) == (int*)(-1)) {
+        printf("Can't attach shared memory for text size\n");
         exit(-1);
     }
-    file_size = *file_size_ptr;
-    source_code = (char*)(file_size_ptr + 1);
+    size = *file_ptr1;
     
-    for (int i = 0; i < file_size; i++)
-        putchar(source_code[i]);
+    if ((shmid2 = shmget(key2, size * sizeof(char), 0)) < 0) {
+        printf("Can\'t create shared memory for text\n");
+        exit(-1);
+    }
+    if ((file_ptr2 = (int*)shmat(shmid2, NULL, 0)) == (int*)(-1)) {
+        printf("Can't attach shared memory for text\n");
+        exit(-1);
+    }
+	
+	// Выводим считанный текст на экран
+    code = (char*)file_ptr2;
+    puts(code);
     
-    if (shmdt(file_size_ptr) < 0) {
+    // Открепляем общую память
+    if (shmdt(file_ptr1)  < 0 ||
+        shmdt(file_ptr2) < 0) {
         printf("Can't detach shared memory\n");
         exit(-1);
     }
     
-    if (shmctl(shmid, IPC_RMID, NULL) < 0) {
+    // Удаляем сегменты общей памяти
+    if (shmctl(shmid1,  IPC_RMID, NULL) < 0 ||
+        shmctl(shmid2, IPC_RMID, NULL) < 0) {
         printf("Can't delete shared memory\n");
         exit(-1);
     }
     return 0;
 }
+
+// Программа работает, т.к. shmget создает сегмент разделяемой памяти, размер которого равен size, округленному до числа вида k * PAGE_SIZE,
+// где k - натуральное
