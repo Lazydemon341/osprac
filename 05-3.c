08237f4ac870de28d44987eaf73680ed79909dbe
Alexander <vlas.s.341@gmail.com>
2021-04-22 20:12:07 +0300

    Fix

--- 05-3.c
+++ 05-3.c
@@ -11,14 +11,14 @@
 	S = 1
 
 	parent:
-		while (n > 0):
+		for (0, n):
 			write
 			D(S, 1)
 			D(S, 1)
 			read
 			
 	child:
-		while (n > 0):
+		for (0, n):
 			Z(S)
 			read
 			write
@@ -33,40 +33,35 @@ int main()
                                // A file with this name must exist in the current directory
 	key_t  key;          // IPC key
 	struct sembuf mybuf; // Structure for specifying operations on a semaphore.
-  
-    //создаем родителя и ребенка
-    int parent[2], child[2], result;
-    
+	
+    int fd[2], result;
     size_t size;
     char  resstring[14];
-    
-    //открываем pipe родителя и ребенка
-    if (pipe(parent) < 0) {
-        printf("Can\'t open parent pipe\n");
-        exit(-1);
-    }
-    
-    if(pipe(child) < 0){
-        printf("Can\'t open child pipe\n");
-    }
 	
-	if ((key = ftok(pathname,0)) < 0) {
-		printf("Can\'t generate key\n");
+    if (pipe(fd) < 0) {
+		printf("Can\'t open pipe\n");
 		exit(-1);
-    }
-	
-	if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-		printf("Can\'t create semaphore set\n");
+	}
+  
+	if((key = ftok(pathname, 0)) < 0){
+		printf("Cannot generate key\n");
 		exit(-1);
 	}
 	
-	// A(S, 1)
-	mybuf.sem_num = 0;
-	mybuf.sem_op  = 1;
-	mybuf.sem_flg = 0;
-	if (semop(semid, &mybuf, 1) < 0) {
-		printf("Can\'t initialize semaphore\n");
+	if((semid = semget(key, 1, 0666|IPC_CREAT|IPC_EXCL)) < 0){
+	  if(errno != EEXIST){
+		printf("Cannot create semaphore\n");
 		exit(-1);
+	  } else if((semid = semget(key, 1, 0)) < 0){
+		  printf("Cannot find semaphore\n");
+		  exit(-1);
+	  }
+	} else {
+		// A(S, 1)
+		if(semop(semid, &bufA, 1) < 0){
+			printf("Cannot init semaphore\n");
+			exit(-1);
+		}
 	}
 	
 	unsigned int N;
@@ -82,16 +77,10 @@ int main()
         
         /* Parent process */
 		
-		while(N > 0){
-		
-			//закрываем на чтение родителя
-			close(parent[0]);
-			//закрываем на запись ребенка
-			close(child[1]);
-			
+		for(int i = 0; i < N; i++){
 			
 			//пишем в родителя
-			size = write(parent[1], "Hello, world!", 14);
+			size = write(fd[1], "Hello, world!", 14);
 			
 			if (size != 14) {
 				printf("Can\'t write all string to pipe\n");
@@ -118,20 +107,17 @@ int main()
 				exit(-1);
 			}
 			
-			size = read(child[0], resstring, 11);
-			if (size != 11) {
+			size = read(fd[0], resstring, 14);
+			if (size != 14) {
 				printf("Can\'t write all string to pipe\n");
 				exit(-1);
 			}
-			close(child[0]);
-			
-			N = N - 1;
 			
 			printf("\nParent read from child: %s\nParent is done working!\n", resstring);
 		}
     } else {
 		
-		while (N > 0){
+		for(int i = 0; i < N; i++){
 			
 			/* Child process */
 			
@@ -143,14 +129,9 @@ int main()
 				printf("Can\'t wait for 0 on semaphore\n");
 				exit(-1);
 			}
-        
-			//закрываем запись родителя
-			close(parent[1]);
-			//закрываем чтение ребенка
-			close(child[0]);
 			
 			//читаем родителя
-			size = read(parent[0], resstring, 14);
+			size = read(fd[0], resstring, 14);
 			
 			if (size < 0) {
 				printf("Can\'t read string from parent!\n");
@@ -160,12 +141,7 @@ int main()
 			printf("Child read from parent: %s\n", resstring);
 			
 			//пишем в ребенка ответ
-			size = write(child[1], "Bye, World!", 11);
-			//закрываем на запись ребенка и на чтение родителя
-			close(child[1]);
-			close(parent[0]);
-			
-			N = N - 1;
+			size = write(fd[1], "Bye, World....", 14);
 			
 			printf("Child is done working!");
 			
